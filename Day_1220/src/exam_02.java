86bd90a92a2b41235bb3592791ba17ed6bfe48bb
MagNus4840 <pokleemam@naver.com>
2022-01-20 02:25:50 +0900

    이클립스에서 깃허브로 연동하는 테스팅용 커밋입니다.

---
+++ Day_1220/src/exam_02.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class exam_02 {
+
+	public static void main(String[] args) {
+		
+		
+		int s = 2;
+		int v = 4;
+		Scanner scn = new Scanner(System.in);
+		
+		String se;
+		System.out.println("이름을 입력하세요.");
+		se = scn.next();
+		System.out.println("이름 : "+se);
+
+		
+		System.out.println("이클립스 연동용 입니다.");
+	}
+
+}
